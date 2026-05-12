2b76bc68f112372e9e9a8e2802d9b58d76472b1e
Ismael Alvarado <ismael.alvaradoperez@ucr.ac.cr>
2026-05-12 03:19:03 +0000

    Creacion de lab3 junto con el primer ejercicio, correcion completad

---
+++ lab3/ejercicio1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int potencia (int base , int exp) {
+int resultado=1;
+while ( exp > 0 ) {
+resultado=resultado*base;
+exp=exp - 1 ;// elimine el int que inicializaba o sobreescribia sin cesar al exp, hacia bucle infinito
+}
+return resultado;
+}
+
+int main ( void ) {
+printf( "2^8 = %d\n", potencia (2, 8));
+printf( "3^4 = %d\n", potencia (3, 4));
+return 0 ;
+}
