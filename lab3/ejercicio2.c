83609a64e82e3552cd12faeb1efaf214cd9a30ba
IsmaelAlvaradoPerez <147348313+Ismael-Alvarado@users.noreply.github.com>
2026-05-18 16:49:22 -0600

    Corregido para modificar

--- lab3/ejercicio2.c
+++ lab3/ejercicio2.c
@@ -4,30 +4,25 @@
 #define SIZE 4
 
 int contar_unos_fila( int m[ ][ SIZE ] , int fila ) ;
-int
-int
-int
-int
+int contar_unos_columna( int m[ ] [ SIZE ] , int col ) ;
+int es_identidad (int m[ ] [ SIZE ] ) ;
+int tiene_fila_completa ( int m[ ] [ SIZE ] ) ;
+int tiene_columna_completa ( int m[ ] [ SIZE ] ) ;
 
-contar_unos_columna( int m[ ] [ SIZE ] , int col ) ;
-e s _ i d e n t i d a d ( i n t m [ ] [ SIZE ] ) ;
-t i e n e _ f i l a _ c o m p l e t a ( i n t m [ ] [ SIZE ] ) ;
-tiene_columna_completa ( i n t m [ ] [ SIZE ] ) ;
-11
-i n t main ( v o i d ) {
-i n t m[ SIZE ] [ SIZE ] = {
-14
+
+
+
+
+
+int main ( void ) {
+int m[ SIZE ] [ SIZE ] = {
 {1 , 0 , 0 , 0} ,
-15
 {0 , 1 , 0 , 0} ,
-16
 {0 , 0 , 1 , 0} ,
-17
 { 0 , 0 , 0 , 1}
-18
 };
-19
+
 /∗ Su i m p l e m e n t a c i o n ∗/
-20
+
 return 0;
-21 }
+}
