f11d05f7d0df8b595c8fb0cae111a2a0426aed6a
Ismael Alvarado <ismael.alvaradoperez@ucr.ac.cr>
2026-05-18 22:34:37 +0000

    Me cansa la maquina virtual, voy a ver si desde git edito en compu normal

---
+++ lab3/ejercicio2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 4
+
+int contar_unos_fila( int m[ ][ SIZE ] , int fila ) ;
+int
+int
+int
+int
+
+contar_unos_columna( int m[ ] [ SIZE ] , int col ) ;
+e s _ i d e n t i d a d ( i n t m [ ] [ SIZE ] ) ;
+t i e n e _ f i l a _ c o m p l e t a ( i n t m [ ] [ SIZE ] ) ;
+tiene_columna_completa ( i n t m [ ] [ SIZE ] ) ;
+11
+i n t main ( v o i d ) {
+i n t m[ SIZE ] [ SIZE ] = {
+14
+{1 , 0 , 0 , 0} ,
+15
+{0 , 1 , 0 , 0} ,
+16
+{0 , 0 , 1 , 0} ,
+17
+{ 0 , 0 , 0 , 1}
+18
+};
+19
+/∗ Su i m p l e m e n t a c i o n ∗/
+20
+return 0;
+21 }
