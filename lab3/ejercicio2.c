d303d87575b428eb61a6e7f52ee511e1896b79ae
IsmaelAlvaradoPerez <147348313+Ismael-Alvarado@users.noreply.github.com>
2026-05-18 20:31:59 -0600

    Funciones terminadas junto a print, falta el random

--- lab3/ejercicio2.c
+++ lab3/ejercicio2.c
@@ -4,18 +4,16 @@
 #define SIZE 4
 
 int contar_unos_fila( int m[ ][ SIZE ] , int fila ) ;
-int contar_unos_columna( int m[ ] [ SIZE ] , int col ) ;
-int es_identidad (int m[ ] [ SIZE ] ) ;
-int tiene_fila_completa ( int m[ ] [ SIZE ] ) ;
-int tiene_columna_completa ( int m[ ] [ SIZE ] ) ;
-
-
+int contar_unos_columna( int m[ ][ SIZE ] , int col ) ;
+int es_identidad (int m[ ][ SIZE ] ) ;
+int tiene_fila_completa ( int m[ ][ SIZE ] ) ;
+int tiene_columna_completa ( int m[ ][ SIZE ] ) ;
 
 
 
 
 int main ( void ) {
-int m[ SIZE ] [ SIZE ] = {
+int m[ SIZE ][ SIZE ] = {
 {1 , 0 , 0 , 0} ,
 {0 , 1 , 0 , 0} ,
 {0 , 0 , 1 , 0} ,
@@ -23,6 +21,74 @@ int m[ SIZE ] [ SIZE ] = {
 };
 
 /∗ Su i m p l e m e n t a c i o n ∗/
+printf ( "Matriz : \n" ) ;
+for ( int i = 0 ; i < SIZE ; i ++ ) {
+    for ( int j = 0 ; j < SIZE ; j ++ ) {
+        printf ( "%d " , m[ i ][ j ] ) ;  
+    }
+printf ( "\n" ) ;
+}
+const char *identidad = es_identidad(m) ? "si" : "no";
+printf ( "Es matriz identidad: %s \n" , identidad ) ;
+const char *columna1 = tiene_columna_completa(m) ? "si" : "no";
+printf ( "Tiene columna completa de unos: %s \n" , columna1 ) ;
+const char *fila1 = tiene_fila_completa(m) ? "si" : "no";
+printf ( "Tiene fila completa de unos: %s \n" , fila1 ) ;
 
 return 0;
+}
+
+int contar_unos_fila( int m[ ][ SIZE ] , int fila ) {
+int i , cont = 0 ;  
+for ( i = 0 ; i < SIZE ; i ++ ) {
+if ( m[ fila ][ i ] == 1 ) {
+cont ++ ;
+}
+}
+return cont ;
+}
+
+int contar_unos_columna( int m[ ][ SIZE ] , int col ) {
+int i , cont = 0 ;  
+for ( i = 0 ; i < SIZE ; i ++ ) {
+if ( m[ i ][ col ] == 1 ) {
+cont ++ ;
+}
+}
+return cont ;
+}
+
+int es_identidad (int m[ ][ SIZE ] ) {
+int i , j ;
+for ( i = 0 ; i < SIZE ; i ++ ) {
+    for ( j = 0 ; j < SIZE ; j ++ ) {
+        if ( i == j && m[ i ][ j ] != 1 ) {
+        return 0 ;
+        }
+        if ( i != j && m[ i ][ j ] != 0 ) {
+        return 0 ;
+        }
+    }
+}
+return 1 ;
+}
+
+int tiene_fila_completa ( int m[ ][ SIZE ] ) {
+int i ;
+for ( i = 0 ; i < SIZE ; i ++ ) {
+if ( contar_unos_fila( m , i ) == SIZE ) {
+return 1 ;
+}
+}
+return 0 ;
+}
+
+int tiene_columna_completa ( int m[ ][ SIZE ] ) {
+int i ;
+for ( i = 0 ; i < SIZE ; i ++ ) {
+if ( contar_unos_columna( m , i ) == SIZE ) {
+return 1 ;
+}
+}
+return 0 ;
 }
