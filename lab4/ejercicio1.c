44156fd30dd29b588a6a06c4390ff30a2bea14ad
Ismael Alvarado <ismael.alvaradoperez@ucr.ac.cr>
2026-06-11 20:40:30 +0000

    Con comentarios y correcion de validacion de dato

--- lab4/ejercicio1.c
+++ lab4/ejercicio1.c
@@ -13,23 +13,23 @@ Params:
 − int ∗result : donde se almacena la longitud encontrada.
  */
 void findLargestLine(int **matrix, int size , int *result) {
-    int tmp=0;
-    int unos=0;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    int tmp=0; //variable temporal que indica el maximo de 1s
+    int unos=0; // contador de unos
+    for (int i = 0; i < size; i++) { //for que va a traves de cada columna
+        for (int j = 0; j < size; j++) { // for que va a traves de cada fila, formato copiado del print
             //printf("%d ", *(*(matrix+ i) + j));
-            if (*(*(matrix+ i) + j)) {
-                unos++;
-                if (unos>tmp) {
+            if (*(*(matrix+ i) + j)) { //condicional que observa si es 1 = TRue
+                unos++; // suma 1 al contador
+                if (unos>tmp) { // si la suma de unos es mayor que el maximo anterior lo guarda
                     tmp=unos;
                 }
             } 
             else {
-                unos=0;
+                unos=0; //cuando ya no observa un 1 reinicia contador
             }
     }
     }
-    *result=tmp;
+    *result=tmp; //guarda valor maximo
  }
 
 /*
@@ -42,10 +42,10 @@ a la matriz creada.
 − int size : numero de filas y columnas.
  ∗/*/
 void allocateMatrix(int ***matrix, int size) {
-    *matrix = malloc(size * sizeof(int *));
+    *matrix = malloc(size * sizeof(int *)); //crea espacio de tamano size para enteros
     if (*matrix == NULL)
         return;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) { // crea espacio para tamano size para enteros en cada espacio anterior
         *(*matrix + i) = malloc(size * sizeof(int));
         if (*(*matrix + i) == NULL)
             return;
@@ -59,10 +59,10 @@ Params:
  − int size : numero de filas y columnas.
 ∗/*/
  void fillMatrix(int **matrix, int size) {
-    srand(time(NULL));
+    srand(time(NULL)); //inicializa semilla para rand
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            *(*(matrix+ i) + j) = rand()%2;
+        for (int j = 0; j < size; j++) { // se mueve a traves de cada elemento de matriz
+            *(*(matrix+ i) + j) = rand()%2; //asigna valor aleatorio con modulo de 2
             }
 
     }
@@ -104,15 +104,16 @@ int main(void) {
     int size , largestLine;
     int **matrix=NULL;
     int validar = 0;
-    do {
+    do { //request size con validacion
         printf("Digite tamano de matriz: ");
         validar = scanf("%d",&size);
-    } while (validar !=0 && size <= 0);
-    allocateMatrix(&matrix, size);
-    fillMatrix(matrix, size);
-    printMatrix(matrix, size);
-    findLargestLine(matrix, size, &largestLine);
-    freeMatrix(matrix, size);
+        while(getchar() != '\n'); //limpia buffer, espacio contaminado de un scan incorrecto, no se muy bien como funciona
+    } while (validar !=1 || size <=0);
+    allocateMatrix(&matrix, size); //asigna espacio de memoria
+    fillMatrix(matrix, size); // llena con variables random binarios la matriz
+    printMatrix(matrix, size); // imprime matriz
+    findLargestLine(matrix, size, &largestLine); //encuentra cadena de 1s mas larga
+    freeMatrix(matrix, size);//libera espacio
     
     /* El estudiante completa: leer size , llamar allocateMatrix ,
     fillMatrix , printMatrix, findLargestLine y freeMatrix. ∗/*/
