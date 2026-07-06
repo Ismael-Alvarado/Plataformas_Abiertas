2f041df73ec25b0c2339233bd230e996ccfc6fc2
IsmaelAlvaradoPerez <147348313+Ismael-Alvarado@users.noreply.github.com>
2026-07-06 14:38:13 -0600

    Se crea header con .h

---
+++ lab6/person.h
@@ -0,0 +1,10 @@
+#ifndef PERSON_H    
+#define PERSON_H
+#include <string>
+class Person {
+private:
+    char name[50]; // Nombre de la persona
+    int age; // Edad de la persona
+    double height; // Altura de la persona en centímetros
+};
+#endif // PERSON_H      
